f07530fa91751feb84c372f173bd411774ba671d
Sandip Dhillon <sandip@phonetica.com>
2021-09-15 14:06:36 +0100

    file import removed

--- src/app/components/example-card/example-card.component.ts
+++ src/app/components/example-card/example-card.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
+//removal of file import test
+//test
+
 import exampleData from "../../../assets/snapshotExample.json"
 import testData2 from "../../../assets/snapshot.json"
 
